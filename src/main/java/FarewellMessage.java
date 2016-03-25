8cb130814e65c00c9332765bd816d9be8b810e39
Jag Sihota <jagsihota@roadrunner.com>
2016-03-24 22:09:01 -0700

    add java file

---
+++ src/main/java/FarewellMessage.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class FarewellMessage {
+
+	static Scanner myscanner = new Scanner(System.in;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to my farewell application.");
+		System.out.println("Could I have your name please: ");
+		String name = myscanner.nextLine();
+		System.out.println("Thank you for using my simple application " + name);
+		System.out.println("Have a good day");
+
+	}
+
+}
